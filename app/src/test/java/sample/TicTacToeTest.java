c6a462747379f1d6a3c76003ffc39b876f0d09fa
Kyotaro Kaneshima <e235727@ie.u-ryukyu.ac.jp>
2023-11-14 00:06:27 +0900

    forth commit

--- app/src/test/java/sample/TicTacToeTest.java
+++ app/src/test/java/sample/TicTacToeTest.java
@@ -23,6 +23,6 @@ public class TicTacToeTest {
     /** 初期ボードの状態確認テスト
      * BoardSizeTest() を参考に、TicTacToe.initalize()を実行した
      * 直後のボード内の要素が全て 'e' であることを確認するテスト。
-     * op
+     * o
      */
 }
